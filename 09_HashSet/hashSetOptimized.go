0fe15ba193b687d1f510c94027b3aa418295a054
root <subhradeepray2017@gmail.com>
2023-01-11 01:55:38 +0530

    Optimized HashSet for Time Complexity

---
+++ 09_HashSet/hashSetOptimized.go
@@ -0,0 +1,44 @@
+package hashset
+
+import "fmt"
+
+type MyHashSet struct {
+	myMap map[interface{}]string
+}
+
+func (s *MyHashSet) add(val int) {
+	_, presentOrNot := s.myMap[val]
+	if !presentOrNot {
+		s.myMap[val] = "Added"
+	}
+}
+
+func (s *MyHashSet) display() {
+	for value := range s.myMap {
+		fmt.Print(value, " ")
+	}
+}
+
+func Start_Optimized() {
+	myMap := make(map[interface{}]string)
+	myHashSet := &MyHashSet{
+		myMap: myMap,
+	}
+	myHashSet.add(10)
+	myHashSet.add(30)
+	myHashSet.add(20)
+	myHashSet.add(50)
+	myHashSet.add(40)
+
+	fmt.Print("First Run  => ")
+	myHashSet.display()
+
+	myHashSet.add(60)
+	myHashSet.add(30)
+	myHashSet.add(20)
+
+	fmt.Print("\nSecond Run => ")
+	myHashSet.display()
+
+	fmt.Println("")
+}
