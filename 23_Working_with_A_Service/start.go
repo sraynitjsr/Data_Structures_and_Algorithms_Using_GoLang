178f29f4c2764fb6097237666323f12c61aed060
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-01-26 23:56:04 +0530

    Created Running Service

--- 23_Working_with_A_Service/start.go
+++ 23_Working_with_A_Service/start.go
@@ -1,5 +1,22 @@
 package workingwithaservice
 
-func Start() {
+import (
+	"fmt"
+	"time"
+)
 
+func doTask(ch chan string, str string) {
+	for {
+		ch <- str
+	}
+}
+
+func Start() {
+	fmt.Println("Inside Running Service Module")
+	myChannel := make(chan string)
+	go doTask(myChannel, "Hello World")
+	for data := range myChannel {
+		fmt.Println("Current Data From Channel is =>", data)
+		time.Sleep(time.Second * 1)
+	}
 }
