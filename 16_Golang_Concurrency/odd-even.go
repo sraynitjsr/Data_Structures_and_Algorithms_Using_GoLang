0c2c85141a9d4c4a6a14f9094fe0129b1ad2f4d7
Subhradeep Ray (Nokia) <subhradeepray2017@gmail.com>
2023-02-12 16:44:20 +0530

    Print Odd Even Numbers Synchronously From Two Go Routines

---
+++ 16_Golang_Concurrency/odd-even.go
@@ -0,0 +1,37 @@
+package golangconcurrency
+
+import (
+	"fmt"
+	"sync"
+)
+
+func sendOdd(num int, wg *sync.WaitGroup, myDoneChannel chan bool) {
+	defer wg.Done()
+	for i := 1; i <= num; i++ {
+		if i%2 != 0 && <-myDoneChannel {
+			fmt.Println("Odd", i)
+			myDoneChannel <- true
+		}
+	}
+}
+
+func sendEven(num int, wg *sync.WaitGroup, myDoneChannel chan bool) {
+	defer wg.Done()
+	for i := 1; i <= num; i++ {
+		if i%2 == 0 && <-myDoneChannel {
+			fmt.Println("Even", i)
+			myDoneChannel <- true
+		}
+	}
+}
+
+func oddEven() {
+	var wg sync.WaitGroup
+	myNumber := 20
+	myDoneChannel := make(chan bool, 1)
+	myDoneChannel <- true
+	wg.Add(2)
+	go sendEven(myNumber, &wg, myDoneChannel)
+	go sendOdd(myNumber, &wg, myDoneChannel)
+	wg.Wait()
+}
--- 16_Golang_Concurrency/start.go
+++ 16_Golang_Concurrency/start.go
@@ -1,5 +1,5 @@
 package golangconcurrency
 
 func Start() {
-
+	oddEven()
 }
