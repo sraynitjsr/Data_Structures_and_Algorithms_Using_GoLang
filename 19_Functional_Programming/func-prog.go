e6526bd64705a3bcf19c5ce73f8b9edb344a946e
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-11-13 16:06:50 +0530

    Adding Predicate Function

--- 19_Functional_Programming/func-prog.go
+++ 19_Functional_Programming/func-prog.go
@@ -1,11 +1,22 @@
 package func_prog
 
 type MapperFunc func(int) int
+type PredicateFunc func(int) bool
 
 func Map(slice []int, mapper MapperFunc) []int {
    result := make([]int, len(slice))
    for i, v := range slice {
       result[i] = mapper(v)
+   }
+   return result
+}
+
+func Filter(slice []int, predicate PredicateFunc) []int {
+   var result []int
+   for _, v := range slice {
+      if predicate(v) {
+         result = append(result, v)
+      }
    }
    return result
 }
