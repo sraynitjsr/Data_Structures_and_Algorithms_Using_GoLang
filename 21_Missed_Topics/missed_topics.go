013d336b52d8139335c0ddc413459b65a47f278f
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-02-08 01:34:58 +0530

    Using Rune in GoLang

--- 21_Missed_Topics/missed_topics.go
+++ 21_Missed_Topics/missed_topics.go
@@ -2,6 +2,28 @@ package missedtopics
 
 import "fmt"
 
+func traverseAString() {
+	myStr := "Some_Dummy_String"
+	for _, v := range myStr {
+		if string(v) == "_" {
+			fmt.Println()
+		} else {
+			fmt.Print(string(v))
+		}
+	}
+}
+
+func usingRuneInGoLang() {
+	mySlice := []rune{}
+	for i := 0; i < 10; i++ {
+		mySlice = append(mySlice, rune(i))
+	}
+	fmt.Println(mySlice)
+}
+
 func Start() {
-	fmt.Println("Topics Missed Previously")
+	fmt.Println("Topics Missed Previously, Runes")
+	traverseAString()
+	fmt.Println()
+	usingRuneInGoLang()
 }
