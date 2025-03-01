7c78f2828986f65fd888c25d1c32948caa3a66ea
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-03-01 23:55:04 +0530

    Interface Embedding

--- 08_Interface/embedding-loose-coupling.go
+++ 08_Interface/embedding-loose-coupling.go
@@ -2,47 +2,65 @@ package interfaces
 
 import "fmt"
 
-type Engine interface {
-	Start()
+type Tax interface {
+	SetIncomeTax(float64)
+	GetIncomeTax() float64
 }
 
-type GasEngine struct {
-	power int
+type IndividualTax struct {
+	incomeTax float64
 }
 
-func (e *GasEngine) Start() {
-	fmt.Println("Gas engine started with power:", e.power)
+func (it *IndividualTax) SetIncomeTax(tax float64) {
+	it.incomeTax = tax
 }
 
-type ElectricEngine struct {
-	power int
+func (it *IndividualTax) GetIncomeTax() float64 {
+	return it.incomeTax
 }
 
-func (e *ElectricEngine) Start() {
-	fmt.Println("Electric engine started with power:", e.power)
+type CorporateTax struct {
+	incomeTax float64
 }
 
-type Car struct {
-	engine Engine
-	brand  string
+func (ct *CorporateTax) SetIncomeTax(tax float64) {
+	ct.incomeTax = tax
 }
 
-func NewCar(brand string, engine Engine) *Car {
-	return &Car{
-		engine: engine,
-		brand:  brand,
+func (ct *CorporateTax) GetIncomeTax() float64 {
+	return ct.incomeTax
+}
+
+type Taxpayer struct {
+	tax  Tax
+	name string
+}
+
+func NewTaxpayer(name string, tax Tax) *Taxpayer {
+	return &Taxpayer{
+		tax:  tax,
+		name: name,
 	}
 }
 
+func (tp *Taxpayer) SetTax(taxToPay float64) {
+	tp.tax.SetIncomeTax(taxToPay)
+}
+
+func (tp *Taxpayer) GetTax() float64 {
+	return tp.tax.GetIncomeTax()
+}
+
 func Embedding() {
-	gasEngine := &GasEngine{power: 200}
-	electricEngine := &ElectricEngine{power: 150}
+	individual := &IndividualTax{}
+	corporate := &CorporateTax{}
 
-	myCar := NewCar("Toyota", gasEngine)
-	fmt.Println("Car brand:", myCar.brand)
-	myCar.engine.Start()
+	individualTaxpayer := NewTaxpayer("Wah Modi Ji", individual)
+	corporateTaxpayer := NewTaxpayer("Nirmala Tai", corporate)
 
-	electricCar := NewCar("Tesla", electricEngine)
-	fmt.Println("Car brand:", electricCar.brand)
-	electricCar.engine.Start()
+	individualTaxpayer.SetTax(99999.99999)
+	corporateTaxpayer.SetTax(1.1)
+
+	fmt.Println("Individual Income Tax:", individualTaxpayer.GetTax())
+	fmt.Println("Corporate Income Tax:", corporateTaxpayer.GetTax())
 }
