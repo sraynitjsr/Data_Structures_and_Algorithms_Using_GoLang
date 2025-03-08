9f2333b1231743446a54baa581889831703754ba
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-03-08 14:42:33 +0530

    Print Odd Even From Two Routines Alternatively Clean Solution

--- 16_Golang_Concurrency/odd-even.go
+++ 16_Golang_Concurrency/odd-even.go
@@ -2,38 +2,62 @@ package golangconcurrency
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 )
 
-func sendOdd(num int, wg *sync.WaitGroup, myDoneChannel chan bool) {
-	defer wg.Done()
-	for i := 1; i <= num; i++ {
-		if i%2 != 0 && <-myDoneChannel {
-			fmt.Println("Odd", i)
-			myDoneChannel <- true
+var (
+	writeWaitGroup sync.WaitGroup
+
+	lock      sync.Mutex
+	condition = sync.NewCond(&lock)
+
+	myNumber  = 10
+	flag      = true
+	myChannel = make(chan string)
+)
+
+func oddWriter() {
+	defer writeWaitGroup.Done()
+	for i := 1; i <= myNumber; i += 2 {
+		lock.Lock()
+		for !flag {
+			condition.Wait()
 		}
+		myChannel <- "Odd  Data => " + strconv.Itoa(i)
+		flag = false
+		condition.Signal()
+		lock.Unlock()
 	}
 }
 
-func sendEven(num int, wg *sync.WaitGroup, myDoneChannel chan bool) {
-	defer wg.Done()
-	for i := 1; i <= num; i++ {
-		if i%2 == 0 && <-myDoneChannel {
-			fmt.Println("Even", i)
-			myDoneChannel <- true
+func evenWriter() {
+	defer writeWaitGroup.Done()
+	for i := 2; i <= myNumber; i += 2 {
+		lock.Lock()
+		for flag {
+			condition.Wait()
 		}
+		myChannel <- "Even Data => " + strconv.Itoa(i)
+		flag = true
+		condition.Signal()
+		lock.Unlock()
 	}
 }
 
 func oddEven() {
 	fmt.Println("Inside Print Odd Even From Two Routines Alternatively Module")
-	var wg sync.WaitGroup
-	myNumber := 10
-	myDoneChannel := make(chan bool, 1)
-	myDoneChannel <- true
-	wg.Add(2)
-	go sendEven(myNumber, &wg, myDoneChannel)
-	go sendOdd(myNumber, &wg, myDoneChannel)
-	wg.Wait()
-}
 
+	writeWaitGroup.Add(2)
+	go oddWriter()
+	go evenWriter()
+
+	go func() {
+		writeWaitGroup.Wait()
+		close(myChannel)
+	}()
+
+	for data := range myChannel {
+		fmt.Println(data)
+	}
+}
