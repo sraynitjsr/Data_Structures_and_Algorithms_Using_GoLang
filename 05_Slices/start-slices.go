cfd524ef78c8e225f4a3831191ed09983e2d801b
Subhradeep Ray <subhradeepray2017@gmail.com>
2025-03-08 15:46:36 +0530

    Append To Modify Refernce

--- 05_Slices/start-slices.go
+++ 05_Slices/start-slices.go
@@ -38,4 +38,6 @@ func Start() {
 	basics()
 
 	tricky.TrickySlice1()
+
+	tricky.TrickySlice2()
 }
---
+++ 05_Slices/tricky/tricky-slice-2.go
@@ -0,0 +1,20 @@
+package tricky
+
+import "fmt"
+
+func trickyCodeAppend(data []int) {
+	data[2] = -1
+	fmt.Println("2 => ", data)
+	data = append(data, 2002) // This changes refernce now for data
+	fmt.Println("3 => ", data)
+	data[3] = 8080
+	fmt.Println("4 => ", data)
+}
+
+func TrickySlice2() {
+	fmt.Println("Tricky SLice Usage 2")
+	data := []int{1, 2, 3, 4, 5}
+	fmt.Println("1 => ", data)
+	trickyCodeAppend(data)
+	fmt.Println("5 => ", data)
+}
